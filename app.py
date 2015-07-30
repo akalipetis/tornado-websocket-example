f1a387054226e5077d00e87c487d975029635fc3
Antonis Kalipetis <akalipetis@gmail.com>
2015-07-30 18:55:32 +0300

    Styling fixes and dynamic host in HTML

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from tornado import websocket, web, ioloop
 import json
+
+from datetime import timedelta
+from tornado import websocket, web, ioloop
+
 
 cl = []
 
+
 class IndexHandler(web.RequestHandler):
     def get(self):
-        self.render("index.html")
+        self.render('index.html')
+
 
 class SocketHandler(websocket.WebSocketHandler):
     def check_origin(self, origin):
@@ -24,9 +29,9 @@ class ApiHandler(web.RequestHandler):
     @web.asynchronous
     def get(self, *args):
         self.finish()
-        id = self.get_argument("id")
-        value = self.get_argument("value")
-        data = {"id": id, "value" : value}
+        id = self.get_argument('id')
+        value = self.get_argument('value')
+        data = {'id': id, 'value' : value}
         data = json.dumps(data)
         for c in cl:
             c.write_message(data)
@@ -36,13 +41,27 @@ class ApiHandler(web.RequestHandler):
         pass
 
 app = web.Application([
-    (r'/', IndexHandler),
-    (r'/ws', SocketHandler),
-    (r'/api', ApiHandler),
-    (r'/(favicon.ico)', web.StaticFileHandler, {'path': '../'}),
-    (r'/(rest_api_example.png)', web.StaticFileHandler, {'path': './'}),
-])
+        (r'/', IndexHandler),
+        (r'/ws', SocketHandler),
+        (r'/api', ApiHandler),
+        (r'/(favicon.ico)', web.StaticFileHandler, {'path': '../'}),
+        (r'/(rest_api_example.png)', web.StaticFileHandler, {'path': './'}),
+    ], debug=True, )
+
+
+def ping_clients():
+    try:
+        for client in cl:
+            client.ping('ping')
+    finally:
+        ioloop.IOLoop.instance().add_timeout(
+            timedelta(seconds=10), ping_clients
+        )
+
 
 if __name__ == '__main__':
-    app.listen(8888)
+    ioloop.IOLoop.instance().add_timeout(
+        timedelta(seconds=10), ping_clients
+    )
+    app.listen(5000)
     ioloop.IOLoop.instance().start()
